1b38f9c48e08c55533142983affc4b9f81019e77
Stefan Mahr <stefan.mahr@sphairon.com>
2013-02-20 20:11:04 +0100

    cleanup

--- legacy/ser_tb/ser_readto.m
+++
@@ -1,8 +0,0 @@
-function outs=ser_readto(fd,readto,timeout)
-
-outs = [];
-for l=1:timeout
-    outs = [outs char(ser_read(fd,1000,100))];
-    if (~isempty(findstr(outs,readto))), break; end;
-end;
-
